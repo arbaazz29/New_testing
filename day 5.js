ecb1a269d4c9fe1d66edd5589159d95de4d9bedd
Arbaz Ansari <arbazansari29a@gmail.com>
2024-08-22 19:15:14 +0530

    Consoling

---
+++ day 5.js
@@ -0,0 +1,27 @@
+//intialise Git Repositry (git init)
+//git add .  (For tracking all files that have changed)
+//git add "file_name"  (for tracking changes ina particular file)
+//git add "file1" "file2" "file3" "file4"
+//tracking environment is known as Staging environment---temporary storage
+
+//to make changes permanent we use--
+//git commit -m 'Your Message'
+//example--git commit -m 'Creating index.html and index.js file'
+
+console.log('Message');
+console.log('H');
+//git status
+
+
+
+
+
+
+
+
+
+
+
+
+
+
